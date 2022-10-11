95e49955b03efdf343570ad6b1cfd0f678b05368
Mateja Čerina <mc5620@student.uni-lj.si>
2022-10-11 14:54:31 +0200

    Added routes

--- airports.py
+++ airports.py
@@ -2,7 +2,7 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
-from shapely.geometry import Point
+from shapely.geometry import Point, LineString
 
 
 def main():
@@ -16,15 +16,47 @@ def main():
 
     airports_geodata = gpd.GeoDataFrame(airports, crs="EPSG:4326", geometry=airports_geometry)
 
-    fig, ax = plt.subplots(subplot_kw={"projection": ccrs.Robinson()},
-                           figsize=(20, 20),
-                           facecolor='black')
+    routes = pd.read_csv("resources/routes", delimiter=",",
+                         names=["airline", "id", "source_airport",
+                                "source_airport_id", "destination_airport",
+                                "destination_airport_id", "codeshare",
+                                "stops", "equitment"])
 
-    ax.patch.set_facecolor('black')
+    source_airports = airports[["name", "iata", "icao", "lat", "long"]]
+    destination_airports = source_airports.copy()
+    source_airports.columns = [str(col) + "_source" for col in source_airports.columns]
+    destination_airports.columns = [str(col) + "_destination" for col in destination_airports.columns]
 
-    airports_geodata.plot(ax=ax, transform=ccrs.PlateCarree(),
-                      markersize=4, edgecolors='none')
+    routes = routes[["source_airport", "destination_airport"]]
 
+    routes = pd.merge(routes,
+                      source_airports,
+                      left_on="source_airport",
+                      right_on="iata_source")
+
+    routes = pd.merge(routes,
+                      destination_airports,
+                      left_on="destination_airport",
+                      right_on="iata_destination")
+
+    routes_geometry = [LineString([[routes.iloc[i]["long_source"],
+                                    routes.iloc[i]["lat_source"]],
+                                   [routes.iloc[i]["long_destination"],
+                                    routes.iloc[i]["lat_destination"]]])
+                       for i in range(routes.shape[0])]
+
+    routes_geodata = gpd.GeoDataFrame(routes, geometry=routes_geometry, crs="EPSG:4326")
+
+    fig, ax = plt.subplots(figsize=(20, 20), subplot_kw={"projection": ccrs.Robinson()})
+    ax.patch.set_facecolor("black")
+
+    routes_geodata.plot(ax=ax, color="white", linewidth=0.1,
+                        transform=ccrs.Geodetic(),
+                        alpha=0.1)
+
+    plt.setp(ax.spines.values(), color="black")
+    plt.setp([ax.get_xticklines(), ax.get_yticklines()], color="black")
+    ax.set_ylim(-7000000, 8800000)
     plt.show()
 
 
